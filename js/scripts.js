ab4a4043cdffb09f5f90ffe454021d1541a5bf6a
Jared Eiseman and Justin Gomez <justin@>
2017-05-02 10:19:45 -0700

    add second question with same checkboxes, change name for second question to non-work-responses, add css to hide new output span of id non-work-responses, adjust JS to target properly for work responses and duplicate for non-work-responses

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,19 @@ $(document).ready(function () {
     console.log(e);
     //this will show the hidden element
     $('#work-responses').show();
+    $('#non-work-responses').show();
     //this targets each checked input
-    $('input[type="checkbox"]:checked').each(function() {
+    $('input[name="work-transportation"]:checked').each(function() {
     //this logs to the console what "this".val() is
       console.log($(this).val());
       //this will append the string value and break tag to work-responses
       $('#work-responses').append($(this).val() + "<br>");
+    });
+    $('input[name="non-work-transportation"]:checked').each(function() {
+    //this logs to the console what "this".val() is
+      console.log($(this).val());
+      //this will append the string value and break tag to work-responses
+      $('#non-work-responses').append($(this).val() + "<br>");
     });
   });
 });
